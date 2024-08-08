4cb234e403b75f17fa2f0864249859ae66f4f3df
Zhao,Hongbo <hongbo.zhao@intel.com>
2024-08-08 21:14:18 +0800

    Update

--- test_ov_moondream2.py
+++ test_ov_moondream2.py
@@ -49,36 +49,40 @@ if __name__ == '__main__':
     version = ov.get_version()
     print("OpenVINO version \n", version)
 
-    vision_start = time.perf_counter()
-    enc_image = moondream2_model.vision_model(picture_path)
-    vision_end = time.perf_counter()
-    vision_infer_time = ((vision_end - vision_start) * 1000)
+    for i in range(2):
+        vision_start = time.perf_counter()
+        enc_image = moondream2_model.vision_model(picture_path)
+        vision_end = time.perf_counter()
+        vision_infer_time = ((vision_end - vision_start) * 1000)
 
-    chat_history=""
-    prompt = f"<image>\n\n{chat_history}Question: {question}\n\nAnswer:"
+        chat_history=""
+        prompt = f"<image>\n\n{chat_history}Question: {question}\n\nAnswer:"
 
 
-    start = time.perf_counter()
-    inputs_embeds=moondream2_model.input_embeds(prompt, enc_image)
-    end = time.perf_counter()
-    embeds_infer_time = ((end - start) * 1000)
+        start = time.perf_counter()
+        inputs_embeds=moondream2_model.input_embeds(prompt, enc_image)
+        end = time.perf_counter()
+        embeds_infer_time = ((end - start) * 1000)
 
-    generate_config = {
-            "eos_token_id": moondream2_model.tokenizer.eos_token_id,
-            "bos_token_id": moondream2_model.tokenizer.bos_token_id,
-            "pad_token_id": moondream2_model.tokenizer.bos_token_id,
-            "max_new_tokens": max_new_tokens,
-        }
-    streamer = TextStreamer(moondream2_model.tokenizer, skip_special_tokens=True, skip_prompt=True)
-    output_ids = moondream2_model.generate( 
-                inputs_embeds=inputs_embeds, **generate_config, streamer=streamer
-            )
-    llm_end = time.perf_counter()
-    
-    #print(moondream2_model.tokenizer.batch_decode(output_ids, skip_special_tokens=True))
-
-    if len(llm_infer_list) > 1:
-        avg_token = sum(llm_infer_list[1:]) / (len(llm_infer_list) - 1)
-        print(f"warm up Inputs len {inputs_embeds.shape[1]}, First token latency: {llm_infer_list[0]:.2f} ms, Output len {len(llm_infer_list) - 1}, Avage token latency: {avg_token:.2f} ms")
-        print(f"visin latency: {vision_infer_time:.2f} ms, embeds latency : {embeds_infer_time:.2f} ms")
-    print("e2e latency: ", sum(llm_infer_list) + vision_infer_time + embeds_infer_time)
+        generate_config = {
+                "eos_token_id": moondream2_model.tokenizer.eos_token_id,
+                "bos_token_id": moondream2_model.tokenizer.bos_token_id,
+                "pad_token_id": moondream2_model.tokenizer.bos_token_id,
+                "max_new_tokens": max_new_tokens,
+            }
+        streamer = TextStreamer(moondream2_model.tokenizer, skip_special_tokens=True, skip_prompt=True)
+        output_ids = moondream2_model.generate( 
+                    inputs_embeds=inputs_embeds, **generate_config, streamer=streamer
+                )
+        llm_end = time.perf_counter()
+        
+        #print(moondream2_model.tokenizer.batch_decode(output_ids, skip_special_tokens=True))
+        
+        ## i= 0 is warming up
+        if i != 0:
+            print("\n\n")
+            if len(llm_infer_list) > 1:
+                avg_token = sum(llm_infer_list[1:]) / (len(llm_infer_list) - 1)
+                print(f"Inputs len {inputs_embeds.shape[1]}, First token latency: {llm_infer_list[0]:.2f} ms, Output len {len(llm_infer_list) - 1}, Avage token latency: {avg_token:.2f} ms")
+                print(f"visin latency: {vision_infer_time:.2f} ms, embeds latency : {embeds_infer_time:.2f} ms")
+            print("e2e latency: ", sum(llm_infer_list) + vision_infer_time + embeds_infer_time)
